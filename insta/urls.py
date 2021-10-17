fc23348f24dde759a21f4fd91be91cd5aee311f4
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 10:25:23 +0300

    create registration folder templates and base file for inheritance

--- insta/urls.py
+++ insta/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^',include('instagram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
