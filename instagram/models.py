b48747e2423b5dc6f009a244f803f3e6880e47aa
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 09:33:14 +0300

    adding profile table methods

--- instagram/models.py
+++ instagram/models.py
@@ -11,8 +11,18 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
     followers = models.ManyToManyField('Profile',related_name="profile_followers",blank=True,default=0)
     following = models.ManyToManyField('Profile',related_name="profile_following",blank=True,default=0)
+    
     def __str__(self):
         return self.user
+
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
+    @classmethod
+    def search_profile(cls,search_term):
+        profiles = cls.objects.filter(user__icontains=search_term)
+        return profiles    
 
 class Image(models.Model):
     image = CloudinaryField('image')
