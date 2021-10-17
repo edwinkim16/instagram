e0d4dfbd53c031dd23f47b846455ced3db59d777
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 10:18:18 +0300

    created comment and update image form

--- instagram/forms.py
+++ instagram/forms.py
@@ -11,3 +11,13 @@ class PostImage(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['owner','post_date','profile']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['user','image','posted_on']
+
+class UpdateImage(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['image','owner','profile','post_date']        
