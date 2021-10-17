f6d316b1e15ea8b426ef624a00be7b5edf908dc4
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 11:48:29 +0300

    setting comment, newimage,likes &update image views and their urls

--- instagram/views.py
+++ instagram/views.py
@@ -59,4 +59,98 @@ def update_profile(request,id):
 def no_profile(request,id):
     
     user = User.objects.get(id=id)
-    return render(request,'profile/no_profile.html',{"user":user})    
+    return render(request,'profile/no_profile.html',{"user":user}) 
+
+def search_results(request):
+    profile = Profile.objects.all
+    
+    if 'user' in request.GET and request.GET["user"]:
+        search_term = request.GET.get("user")
+        
+        searched_users = User.objects.filter(username__icontains=search_term)
+        
+        message = f"{search_term}"
+        
+        return render(request, 'searched.html',{"message":message,"users": searched_users,"profile":profile})
+
+    else:
+        message = "Please input a name in the search form"
+        return render(request, 'searched.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def new_image(request,id):
+    current_user = request.user
+    try:
+        current_profile = Profile.objects.get(user_id=id)
+    except ObjectDoesNotExist:
+        return redirect(update_profile,current_user.id)
+        
+    if request.method == 'POST':
+        form = PostImage(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.owner = current_user
+            image.profile = current_profile
+            image.save()
+        return redirect(home)
+    else:
+        form = PostImage()
+        
+    return render(request, 'new_image.html', {'user':current_user,"form": form})
+
+@login_required(login_url='/accounts/login/')   
+def comment(request,c_id):
+    comments = Comment.objects.filter(image_id=c_id)
+    current_user = request.user
+    current_image = Image.objects.get(id=c_id)
+    try:
+        likes = Like.objects.filter(post_id=c_id).count()
+    except ObjectDoesNotExist:
+        likes =0
+    try:
+        like = Like.objects.filter(post_id=c_id).get(user_id=request.user)
+    except ObjectDoesNotExist:
+        like =0
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = current_image
+            comment.user = current_user   
+            comment.save()
+            return redirect(home)
+    else:
+        form = CommentForm()
+        
+    return render(request,'comments.html',{"form":form,'comments':comments,"image":current_image,"user":current_user,'like':like,"likes":likes})   
+
+def like_pic(request, pic_id):
+    current_user = request.user
+    
+    image = Image.objects.get(id=pic_id)
+    new_like = Like(post=image,user=current_user)
+    new_like.save()
+    return redirect(comment,image.id)
+
+def follow(request,user_id):
+    current_user = request.user
+    to_follow = Profile.objects.get(user_id=user_id)
+    new_profile = Profile(user_id=to_follow,followers=current_user.id,following=to_follow)
+    new_profile.save()
+    return redirect(home)  
+
+@login_required(login_url='/accounts/login/')
+def update_image(request,id):
+    current_user = request.user
+    image = Image.objects.get(id=id)
+    if request.method == 'POST':
+        form = UpdateImage(request.POST)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.owner = current_user
+            # image.update_image(current,new)
+            return redirect(home)
+    else:
+        form = UpdateImage()
+
+    return render(request,'update_image.html',{'user':current_user,'form':form,"image":image})
