ccd5678d068e1659001057d1b26de5d2fa1e5954
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 11:35:49 +0300

    setting up first view and debugging it

--- instagram/models.py
+++ instagram/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.db.models.fields import related
+import datetime as dt
 
 # Create your models here.
 
@@ -9,11 +10,11 @@ class Profile(models.Model):
     bio = models.CharField(max_length =200)
     profile_pic = CloudinaryField('image')
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
-    followers = models.ManyToManyField('Profile',related_name="profile_followers",blank=True,default=0)
-    following = models.ManyToManyField('Profile',related_name="profile_following",blank=True,default=0)
+    followers = models.ManyToManyField('Profile',related_name="profile_followers",blank=True,default=[0])
+    following = models.ManyToManyField('Profile',related_name="profile_following",blank=True,default=[0])
     
     def __str__(self):
-        return self.user
+        return self.user.username
 
     def save_profile(self):
         self.save()
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from .models import Image,Profile,Like,Followers,Comment
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import UpdateProfileForm,PostImage,CommentForm,UpdateImage
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to Instagram')
+
+def home(request):
+    images = Image.objects.all().order_by('-post_date')
+    users = User.objects.all()  
+    current = request.user
+    likes = Like.objects.all().count()
+    
+    return render(request, 'index.html',{"images":images,'users':users,'current':current,"likes":likes})    
