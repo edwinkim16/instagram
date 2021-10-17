1c1648625b3ef4b707d79daac3d8d9dfa6fb5b60
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 11:42:39 +0300

    setting profile views

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 urlpatterns=[
     #url('^$',views.welcome,name = 'welcome'),
     url(r'^$',views.home,name='home'),
+    url(r'^profile/(\d+)$',views.profile,name='profile'),
+    url(r'^profile/update/(\d+)$',views.update_profile,name='update_profile'),
+    url(r'no profile/(\d+)',views.no_profile),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -17,4 +17,46 @@ def home(request):
     current = request.user
     likes = Like.objects.all().count()
     
-    return render(request, 'index.html',{"images":images,'users':users,'current':current,"likes":likes})    
+    return render(request, 'index.html',{"images":images,'users':users,'current':current,"likes":likes})   
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+    current_user = request.user
+    user = User.objects.get(id=id)
+    if current_user.id == user.id:
+        images = Image.objects.filter(owner_id=id)
+        current_user = request.user
+        user = User.objects.get(id=id)
+        try:
+            profile = Profile.objects.get(user_id=id)
+        except ObjectDoesNotExist:
+            return redirect(update_profile,current_user.id)
+    else: 
+        try:
+            profile = Profile.objects.get(user_id=id)
+        except ObjectDoesNotExist:
+            
+            return redirect(no_profile,id)      
+            
+    return render(request,'profile/profile.html',{'user':user,'profile':profile,'images':images,'current_user':current_user})   
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request,id):
+    
+    current_user = request.user
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user_id=id
+            profile.save()
+            return redirect(home)
+    else:
+        form = UpdateProfileForm()
+    return render(request,'profile/update_profile.html',{'user':user,'form':form})      
+
+def no_profile(request,id):
+    
+    user = User.objects.get(id=id)
+    return render(request,'profile/no_profile.html',{"user":user})    
