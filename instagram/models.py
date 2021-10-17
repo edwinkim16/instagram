8fd085c8581a39986305471a6da6d758a49535b2
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-17 09:37:32 +0300

    creating image methods

--- instagram/models.py
+++ instagram/models.py
@@ -32,6 +32,26 @@ class Image(models.Model):
     caption = models.CharField(max_length =50)
     post_date = models.DateTimeField(auto_now_add=True)
     
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+    
+
+    @classmethod
+    def get_all_images(cls):
+        all_images = Image.objects.all()
+        for image in all_images:
+            return image
+
+    @classmethod
+    def update_image(cls,current,new):
+        to_update = Image.objects.filter(image_name=current).update(image_name=new)
+        return to_update
+    @classmethod
+    def get_image_by_id(cls,id):
+        image_result = cls.objects.get(id=id)
+        return image_result
      
     def __str__(self):
         return self.image_name        
